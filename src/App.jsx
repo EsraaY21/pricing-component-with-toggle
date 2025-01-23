bfa6813969a061e53a24111bef47d759c25575d2
Esra <esraayaareb@gmail.com>
2025-01-23 16:08:17 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+function App() {
+  return (
+    <>
+      <main>
+        <section className="pricing_section">
+          <h1>Our Pricing</h1>
+          <div className="pricing_filter">
+            <p>Annually</p>
+
+            <p>Monthly</p>
+          </div>
+
+          <div>
+            <article></article>
+            <article></article>
+            <article></article>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
+// Our Pricing
+// Annually
+// Monthly
+
+// Basic
+// &dollar;19.99
+// &dollar;199.99
+// 500 GB Storage
+// 2 Users Allowed
+// Send up to 3 GB
+// Learn More
+
+// Professional
+// &dollar;24.99
+// &dollar;249.99
+// 1 TB Storage
+// 5 Users Allowed
+// Send up to 10 GB
+// Learn More
+
+// Master
+// &dollar;39.99
+// &dollar;399.99
+// 2 TB Storage
+// 10 Users Allowed
+// Send up to 20 GB
+// Learn More
